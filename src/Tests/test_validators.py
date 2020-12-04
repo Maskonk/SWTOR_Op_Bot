b2aa8681718e96775f92904bd077ec52b6ab12be
Kyle Walker <Randomer44@hotmail.com>
2020-12-04 23:09:03 +0000

    Update time validator tests

--- src/Tests/test_validators.py
+++ src/Tests/test_validators.py
@@ -8,31 +8,34 @@ class TestValidators(TestCase):
         pass
 
     def test_validate_time_input_valid(self):
-        assert(Validators.validate_time_input("11/12/2021", "11:20"), True)
+        self.assertTrue(Validators.validate_time_input("11/12/2021", "11:20"))
 
     def test_validate_time_input_invalid(self):
-        assert(Validators.validate_time_input("11/12/19", "11:20"), True)
+        self.assertFalse(Validators.validate_time_input("11/12/19", "11:20"))
 
-    def test_validate_difficulty_input(self):
-        self.fail()
+    def test_validate_time_input_bad_format(self):
+        self.assertFalse(Validators.validate_time_input("Blueberry", "11:20"))
 
-    def test_validate_role(self):
-        self.fail()
-
-    def test_validate_side_input(self):
-        self.fail()
-
-    def test_validate_size_input(self):
-        self.fail()
-
-    def test_validate_operation_input(self):
-        self.fail()
-
-    def test_validate_operation_channel(self):
-        self.fail()
-
-    def test_validate_sign_up_channel(self):
-        self.fail()
-
-    def test_validate_swtor_channel(self):
-        self.fail()
+    # def test_validate_difficulty_input(self):
+    #     self.fail()
+    #
+    # def test_validate_role(self):
+    #     self.fail()
+    #
+    # def test_validate_side_input(self):
+    #     self.fail()
+    #
+    # def test_validate_size_input(self):
+    #     self.fail()
+    #
+    # def test_validate_operation_input(self):
+    #     self.fail()
+    #
+    # def test_validate_operation_channel(self):
+    #     self.fail()
+    #
+    # def test_validate_sign_up_channel(self):
+    #     self.fail()
+    #
+    # def test_validate_swtor_channel(self):
+    #     self.fail()
