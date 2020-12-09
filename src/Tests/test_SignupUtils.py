408a5740749ee7bca90e192d7b46a3d366c334dc
Maskonk <Randomer44@hotmail.com>
2020-12-09 00:08:49 +0000

    Add base test file for SignupUtils

---
+++ src/Tests/test_SignupUtils.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestSignupUtils(TestCase):
+    def test_check_duplicate(self):
+        self.fail()
+
+    def test_check_role_change(self):
+        self.fail()
