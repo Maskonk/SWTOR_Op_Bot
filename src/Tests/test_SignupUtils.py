862cb99e4d51da2cefe13f257cd88db4fbac3c80
Maskonk <Randomer44@hotmail.com>
2020-12-10 05:51:13 +0000

    Add tests for checking role change method

--- src/Tests/test_SignupUtils.py
+++ src/Tests/test_SignupUtils.py
@@ -36,5 +36,42 @@ class TestSignupUtils(TestCase):
         loop.close()
         self.assertTrue(result)
 
-    # def test_check_role_change(self):
-    #     self.fail()
+    def test_check_duplicate_false(self):
+        ops = {"Sign-ups": {"Tank": [], "Dps": [], "Healer": [], "Reserve": [],
+                            "Alternate_Tank": [], "Alternate_Dps": [], "Alternate_Healer": []}}
+        loop = asyncio.new_event_loop()
+        result = loop.run_until_complete(SignupUtils.check_duplicate(ops, "Test"))
+        loop.close()
+        self.assertFalse(result)
+
+    def test_check_role_change_main_role_no_alt_role(self):
+        ops = {"Sign-ups": {"Tank": [], "Dps": [], "Healer": ["Test"], "Reserve": ["Test"],
+                            "Alternate_Tank": [], "Alternate_Dps": [], "Alternate_Healer": []}}
+        loop = asyncio.new_event_loop()
+        result = loop.run_until_complete(SignupUtils.check_role_change(ops, "Test", "Tank", ""))
+        loop.close()
+        self.assertTrue(result)
+
+    def test_check_role_change_alt_role_no_main_change(self):
+        ops = {"Sign-ups": {"Tank": [], "Dps": [], "Healer": ["Test"], "Reserve": ["Test"],
+                            "Alternate_Tank": [], "Alternate_Dps": ["Test"], "Alternate_Healer": []}}
+        loop = asyncio.new_event_loop()
+        result = loop.run_until_complete(SignupUtils.check_role_change(ops, "Test", "Healer", "Tank"))
+        loop.close()
+        self.assertTrue(result)
+
+    def test_check_role_change_both_roles_change(self):
+        ops = {"Sign-ups": {"Tank": [], "Dps": [], "Healer": ["Test"], "Reserve": ["Test"],
+                            "Alternate_Tank": [], "Alternate_Dps": ["Test"], "Alternate_Healer": []}}
+        loop = asyncio.new_event_loop()
+        result = loop.run_until_complete(SignupUtils.check_role_change(ops, "Test", "Tank", "Tank"))
+        loop.close()
+        self.assertTrue(result)
+
+    def test_check_role_change_no_change(self):
+        ops = {"Sign-ups": {"Tank": [], "Dps": [], "Healer": ["Test"], "Reserve": ["Test"],
+                            "Alternate_Tank": [], "Alternate_Dps": ["Test"], "Alternate_Healer": []}}
+        loop = asyncio.new_event_loop()
+        result = loop.run_until_complete(SignupUtils.check_role_change(ops, "Test", "Healer", "Dps"))
+        loop.close()
+        self.assertFalse(result)
