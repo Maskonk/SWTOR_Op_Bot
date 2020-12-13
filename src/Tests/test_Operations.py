21c69b9d59bae5899fc28fc4a66fa39722277340
Maskonk <Randomer44@hotmail.com>
2020-12-13 07:37:39 +0000

    Comment out unfinished tests

--- src/Tests/test_Operations.py
+++ src/Tests/test_Operations.py
@@ -4,29 +4,29 @@ from ..Cogs.Operations import Operations
 
 
 class TestOperations(TestCase):
-    def test_add_signup(self):
-        self.fail()
-
-    def test_add_any_signup(self):
-        self.fail()
-
-    def test_add_reserve(self):
-        self.fail()
-
-    def test_remove_signup(self):
-        self.fail()
-
-    def test_is_owner_or_admin(self):
-        self.fail()
-
-    def test_parse_date(self):
-        self.fail()
-
-    def test_date_extension(self):
-        self.fail()
-
-    def test_check_role_full(self):
-        self.fail()
-
-    def test_add_to_operation(self):
-        self.fail()
+    # def test_add_signup(self):
+    #     self.fail()
+    #
+    # def test_add_any_signup(self):
+    #     self.fail()
+    #
+    # def test_add_reserve(self):
+    #     self.fail()
+    #
+    # def test_remove_signup(self):
+    #     self.fail()
+    #
+    # def test_is_owner_or_admin(self):
+    #     self.fail()
+    #
+    # def test_parse_date(self):
+    #     self.fail()
+    #
+    # def test_date_extension(self):
+    #     self.fail()
+    #
+    # def test_check_role_full(self):
+    #     self.fail()
+    #
+    # def test_add_to_operation(self):
+    #     self.fail()
